bfc8eb7cf9a1856d2836c8ea5f3ba60bb7319c46
Darvetra <darvetra13@gmail.com>
2021-07-28 21:25:03 +0300

    добавляет модуль «Сортировка»

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import {createTripInfoTemplate} from './view/trip-info.js';
 import {createTripPriceInfoTemplate} from './view/trip-price.js';
 import {createSiteMenuTemplate} from './view/site-menu.js';
 import {createFilterTemplate} from './view/filter.js';
+import {createSortTemplate} from './view/sort.js';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const sitePageHeaderElement = document.querySelector('.page-header');
+const sitePageMainElement = document.querySelector('.page-main');
 
+// Хэдер
 // Отрисовывает информацию о поездке (маршрут и дата)
 const tripMainElement = sitePageHeaderElement.querySelector('.trip-main');
 render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
@@ -24,3 +27,8 @@ render(tripControlsNavigationElement, createSiteMenuTemplate(), 'beforeend');
 // Отрисовывает фильтр
 const tripControlsFiltersElement = sitePageHeaderElement.querySelector('.trip-controls__filters');
 render(tripControlsFiltersElement, createFilterTemplate(), 'beforeend');
+
+// Основная часть
+// Отрисовывает сортировку
+const tripEventsElement = sitePageMainElement.querySelector('.trip-events');
+render(tripEventsElement, createSortTemplate(), 'beforeend');

7fc8a8f6abc700004fd7f8a9c095a47700aaa4de
Darvetra <darvetra13@gmail.com>
2021-07-28 20:24:05 +0300

    добавляет модуль «Информация о маршруте (стоимость поездки)»

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {createTripInfoTemplate} from './view/trip-info.js';
+import {createTripPriceInfoTemplate} from './view/trip-price.js';
 import {createSiteMenuTemplate} from './view/site-menu.js';
 
 const render = (container, template, place) => {
@@ -7,9 +8,13 @@ const render = (container, template, place) => {
 
 const sitePageHeaderElement = document.querySelector('.page-header');
 
-// Отрисовывает информацию о поездке (маршрут, стоимость и дата)
+// Отрисовывает информацию о поездке (маршрут и дата)
 const tripMainElement = sitePageHeaderElement.querySelector('.trip-main');
 render(tripMainElement, createTripInfoTemplate(), 'afterbegin');
+
+// Отрисовывает информацию о поездке (стоимость поездки)
+const tripInfoElement = sitePageHeaderElement.querySelector('.trip-info');
+render(tripInfoElement, createTripPriceInfoTemplate(), 'beforeend');
 
 // Отрисовывает меню
 const siteMenuElement = sitePageHeaderElement.querySelector('.trip-controls__navigation');

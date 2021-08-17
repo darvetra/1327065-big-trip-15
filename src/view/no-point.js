73123d64c8dd29baaf211ba3a4b5c7e593d95fb9
Darvetra <darvetra13@gmail.com>
2021-08-17 21:08:07 +0300

    добавляет наследование простым компонентам

--- src/view/no-point.js
+++ src/view/no-point.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createNoPointTemplate = () => (
   `<p class="trip-events__msg">Click New Event to create your first point</p>
@@ -11,24 +11,8 @@ const createNoPointTemplate = () => (
     -->`
 );
 
-export default class NoPoint {
-  constructor() {
-    this._element = null;
-  }
-
+export default class NoPoint extends AbstractView {
   getTemplate() {
     return createNoPointTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }

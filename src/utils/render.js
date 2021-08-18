f075e50ddad2485fc64cc0f9c4625411df2a518f
Darvetra <darvetra13@gmail.com>
2021-08-18 22:15:02 +0300

    создаёт метод remove - обёртку для вызова методов удаления

--- src/utils/render.js
+++ src/utils/render.js
@@ -74,3 +74,16 @@ export const replace = (newChild, oldChild) => {
 
   parent.replaceChild(newChild, oldChild);
 };
+
+/**
+ * Обёртка для вызова методов удаления
+ * @param component
+ */
+export const remove = (component) => {
+  if (!(component instanceof Abstract)) {
+    throw new Error('Can remove only components');
+  }
+
+  component.getElement().remove();
+  component.removeElement();
+};

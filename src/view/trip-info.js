5e7d7e5da9d66507cfc20b75f41f229e7a7bec4e
Darvetra <darvetra13@gmail.com>
2021-08-28 11:58:02 +0300

    рефакторит функцию createTripInfo

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -2,14 +2,14 @@ import AbstractView from './abstract.js';
 
 const createTripInfoTemplate = (tripInfo = {}) => {
   const {
-    travelStartDate,
-    travelEndDate,
+    generalStartDate,
+    generalEndDate,
   } = tripInfo;
 
   return `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
       <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-      <p class="trip-info__dates">${travelStartDate}&nbsp;&mdash;&nbsp;${travelEndDate}</p>
+      <p class="trip-info__dates">${generalStartDate}&nbsp;&mdash;&nbsp;${generalEndDate}</p>
     </div>
   </section>`;
 };

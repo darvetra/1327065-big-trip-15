6ac8074fec73f468e8e3116938276580c353a757
Darvetra <darvetra13@gmail.com>
2021-09-03 08:58:00 +0300

    исправляет функции сравнения (compareFunction) для метода сортировки и заставляет сортировку наконец-то работать

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,17 +1,50 @@
 import dayjs from 'dayjs';
 import {calculateMinuteDiff} from './date.js';
 
-export const sortByDate = (pointA, pointB) => dayjs(pointB.dueDate).diff(dayjs(pointA.dueDate));
+/**
+ * Функция для метода sort
+ * Сортировка по дате
+ * @param pointA
+ * @param pointB
+ */
+export const sortByDate = (pointA, pointB) => dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom));
 
-export const sortByTime = (pointA, pointB) => calculateMinuteDiff(pointA, pointB);
+/**
+ * Функция для метода sort
+ * Сортировка по длительности поездки
+ * @param pointA
+ * @param pointB
+ * @returns {number}
+ */
+export const sortByTime = (pointA, pointB) => {
+  const durationPointA = calculateMinuteDiff(pointA.dateFrom, pointA.dateTo);
+  const durationPointB = calculateMinuteDiff(pointB.dateFrom, pointB.dateTo);
 
-export const sortByPrice = (pointA, pointB) => {
-  if (pointA.basePrice > pointB.basePrice) {
+  if (durationPointA > durationPointB) {
+    return -1;
+  }
+
+  if (durationPointA < durationPointB) {
     return 1;
   }
 
-  if (pointA.basePrice < pointB.basePrice) {
+  return 0;
+};
+
+/**
+ * Функция для метода sort
+ * Сортировка по цене
+ * @param pointA
+ * @param pointB
+ * @returns {number}
+ */
+export const sortByPrice = (pointA, pointB) => {
+  if (pointA.basePrice > pointB.basePrice) {
     return -1;
+  }
+
+  if (pointA.basePrice < pointB.basePrice) {
+    return 1;
   }
 
   return 0;
--- src/view/sort.js
+++ src/view/sort.js
@@ -3,8 +3,8 @@ import {SortType} from '../const.js';
 
 const createSortTemplate = () => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    <div class="trip-sort__item  trip-sort__item--day" data-sort-type="${SortType.DAY}">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+    <div class="trip-sort__item  trip-sort__item--day">
+      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" data-sort-type="${SortType.DAY}" checked>
       <label class="trip-sort__btn" for="sort-day">Day</label>
     </div>
 
@@ -13,13 +13,13 @@ const createSortTemplate = () => (
       <label class="trip-sort__btn" for="sort-event">Event</label>
     </div>
 
-    <div class="trip-sort__item  trip-sort__item--time" data-sort-type="${SortType.TIME}">
-      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+    <div class="trip-sort__item  trip-sort__item--time">
+      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" data-sort-type="${SortType.TIME}">
       <label class="trip-sort__btn" for="sort-time">Time</label>
     </div>
 
-    <div class="trip-sort__item  trip-sort__item--price" data-sort-type="${SortType.PRICE}">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+    <div class="trip-sort__item  trip-sort__item--price">
+      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" data-sort-type="${SortType.PRICE}">
       <label class="trip-sort__btn" for="sort-price">Price</label>
     </div>
 
@@ -42,7 +42,7 @@ export default class Sort extends AbstractView {
   }
 
   _sortTypeChangeHandler(evt) {
-    if (evt.target.tagName !== 'DIV') {
+    if (evt.target.tagName !== 'INPUT') {
       return;
     }
 
